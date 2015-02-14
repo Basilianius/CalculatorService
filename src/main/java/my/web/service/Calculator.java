c8a608160823d7addaaf6d9ebba9451b8e28a937
Basilianius <basilianius@gmail.com>
2015-02-14 23:07:04 +0300

    first commit

---
+++ src/main/java/my/web/service/Calculator.java
@@ -0,0 +1,21 @@
+package my.web.service;
+
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by Basilianius on 14.02.2015.
+ */
+@WebService
+public class Calculator {
+
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:8080/WS/calc", new Calculator());
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+}
